fd1880fc821452c2e2f4987a2ab8e049a03ddb71
HenriqueLimas <henrique.ramos.limas@gmail.com>
2025-03-29 12:50:46 -0700

    use netlify adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
+import marko from "@marko/run/vite";
+import netlify from "@marko/run-adapter-netlify";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    marko({
+      adapter: netlify({ edge: true})
+    })
   ],
 })
