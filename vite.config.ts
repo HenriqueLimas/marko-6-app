df4015516c03f736575e6f036cfa56fea3e72e4f
HenriqueLimas <henrique.ramos.limas@gmail.com>
2025-03-31 08:44:16 -0700

    Use normal css instead of tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import marko from "@marko/run/vite";
 import netlify from "@marko/run-adapter-netlify";
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     marko({
       adapter: netlify({ edge: true})
     })
